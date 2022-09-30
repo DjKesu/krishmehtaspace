b3f0e0a6b6f9a445c18fb024612f88be06e2c55f
Krish Mehta <djkesu@djkesu.space>
2022-09-30 17:29:20 -0400

    Added initial pages

---
+++ src/pages/blogs.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const BlogsPage = () => {
+  return (
+    <main>
+      <h1>Blogs</h1>
+      <p>Here is where I occasionally blog</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default BlogsPage
